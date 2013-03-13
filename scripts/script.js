c9a6e39cae5288ea8b754468f044b613ef8e93b1
Jakob Kallin <jakobkallin>
2013-03-13 18:50:41 +0100

    Angular without theme selector

--- scripts/script.js
+++ scripts/script.js
@@ -1,76 +1,80 @@
-var viewModel = {
-	maxNameCount: 20,
-	gender: ko.observable('male'),
-	lists: {},
-	names: ko.observableArray(),
-	buttonText: ko.observable('Loading Names…'),
-	namesLoaded: ko.observable(false),
-	generateName: function() {
-		var first = this.lists['American'][this.gender()].random();
-		var last = this.lists['American'].family.random();
+Array.prototype.random = function() {
+	return this[Math.floor(Math.random() * this.length)];
+};
+
+var Controller = function($scope) {
+	$scope.maxNameCount = 20;
+	$scope.gender = 'male';
+	$scope.selectedList = null;
+	$scope.lists = [];
+	$scope.names = [];
+	$scope.buttonText = 'Loading Names…';
+	$scope.namesLoaded = false;
+	
+	$scope.generateName = function() {
+		var first = $scope.selectedList[$scope.gender].random();
+		var last = $scope.selectedList.family.random();
 		var full = first + ' ' + last;
 		
-		this.names.unshift(full);
-	},
-	themes: [
+		$scope.names.unshift(full);
+	};
+	
+	$scope.themes = [
 		{ title: 'Generic', path: 'themes/generic/generic.css' },
 		{ title: 'Sci-Fi', path: 'themes/sci-fi/sci-fi.css' },
 		{ title: 'Cthulhu', path: 'themes/cthulhu/cthulhu.css' }
-	],
-	selectTheme: function(data, event) {
+	];
+	
+	$scope.selectTheme = function(data, event) {
 		var path = event.target.value;
 		this.activateStylesheet(path);
-	},
-	activateStylesheet: function(path) {
+	};
+	
+	$scope.activateStylesheet = function(path) {
 		var links = document.head.querySelectorAll('link[title][rel~="stylesheet"]');
 		for ( var i = 0; i < links.length; i++ ) {
 			links[i].disabled = true;
 		}
 		var activeLink = document.head.querySelector('link[href="' + path + '"]');
 		activeLink.disabled = false;
-	}
-};
-
-window.addEventListener('load', function() {
-	ko.applyBindings(viewModel);
-});
-
-Array.prototype.random = function() {
-	return this[Math.floor(Math.random() * this.length)];
-};
-
-window.addEventListener('load', function() {
-	var listNames = ['male', 'female', 'last'];
-	var button = document.getElementsByTagName('button')[0];
+	};
 	
-	loadLists();
-	activateHotkeys();
-	
-	function loadLists() {
-		var fileName = 'stats/american.json';
-		var request = new XMLHttpRequest();
-		request.open('GET', fileName);
-		request.send();
-		request.addEventListener('load', function() {
-			var list = JSON.parse(request.responseText);
-			viewModel.lists[list.group] = list;
-			viewModel.namesLoaded(true);
-			viewModel.buttonText('Generate Name');
-		});
-	}
-	
-	function activateHotkeys() {
-		var hotkeys = {
-			'M': 'male',
-			'F': 'female'
-		};
+	window.addEventListener('load', function() {
+		var listNames = ['male', 'female', 'last'];
+		var button = document.querySelector('button');
 		
-		document.addEventListener('keydown', function(event) {
-			var key = String.fromCharCode(event.keyCode);
-			if ( key in hotkeys ) {
-				viewModel.gender(hotkeys[key]);
-				button.click();
-			}
-		});
-	}
-});
+		loadLists();
+		activateHotkeys();
+		
+		function loadLists() {
+			var fileName = 'stats/american.json';
+			var request = new XMLHttpRequest();
+			request.open('GET', fileName);
+			request.send();
+			request.addEventListener('load', function() {
+				$scope.$apply(function() {
+					var list = JSON.parse(request.responseText);
+					$scope.lists.push(list);
+					$scope.selectedList = list;
+					$scope.namesLoaded = true;
+					$scope.buttonText = 'Generate Name';
+				});
+			});
+		}
+		
+		function activateHotkeys() {
+			var hotkeys = {
+				'M': 'male',
+				'F': 'female'
+			};
+			
+			document.addEventListener('keydown', function(event) {
+				var key = String.fromCharCode(event.keyCode);
+				if ( key in hotkeys ) {
+					$scope.gender = hotkeys[key];
+					button.click();
+				}
+			});
+		}
+	});
+};
